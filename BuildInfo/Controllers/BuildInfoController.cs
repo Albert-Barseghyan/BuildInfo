baa0c4441082dd8a23502296ead375649c260675
Albert-Barseghyan <88934922+Albert-Barseghyan@users.noreply.github.com>
2022-02-15 22:29:39 +0400

    a

---
+++ BuildInfo/Controllers/BuildInfoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models;
+using ServiceLogic.Servicies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildInfo.Controllers
+{
+    public class BuildInfoController : Controller
+    {
+        public List<BuildingDistrictModel> GetDistricts()
+        {
+            InfoService Inf = new InfoService();
+            return null;
+        }
+    }
+}
